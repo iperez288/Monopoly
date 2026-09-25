49c0b3ebf01b9008346a3867fb2b54c06d4a2c69
iperez288 <ikerperezanza@gmail.com>
2026-09-25 15:05:49 +0200

    Card modificado_v2

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//actualizacion viernes 25 is2 verision2
+//actualizacion viernes 25 is2 verision3
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
